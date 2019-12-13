937fd37658bcc8c5ffbf88233387ffd8169cb463
Eevan <eevan.redon@gmail.com>
2019-12-13 01:09:05 +0100

    feature tests cypress

--- cypress/integration/visitor_can_see_landing_page.spec.js
+++ cypress/integration/visitor_can_see_landing_page.spec.js
@@ -3,6 +3,22 @@ describe('Visitor can see landing page', () => {
     cy.visit('/')
 
     cy.get(".divTop")
-        .should("contain", "Rock Paper Scissors")      
+        .should("contain", "Rock Paper Scissors")    
+        .and("contain", "Play the game")  
+
+    cy.get("#toBack")
+      .should("contain", " Choose your Weapon")
+      .and("contain", "is waiting... TO DESTROY YOU")
+
+    cy.get("#toBack").contains("Rock").click()
+    cy.get("#toBack").contains("Paper").click()
+    cy.get("#toBack").contains("Scissors").click()
+    
+    cy.get(".divbottom")
+      .should("contain", "Computer's Score!")
+      .and("contain", "Your Score!")
+
+    cy.get(".divbottom").contains("Reset").click()
+
   });
 })
